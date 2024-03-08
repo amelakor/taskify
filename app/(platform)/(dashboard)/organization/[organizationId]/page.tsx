6c52b920facbd46135fb58d4310a252aa0a1707e
amelakor <amela_korjenic@hotmail.com>
2024-03-08 13:13:09 +0100

    Board list

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,13 +1,17 @@
-"use client";
-import { Form } from "./form";
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
 
-const OrganizationPage = () => {
+const OrganizationIdPage = () => {
     return (
-        <div>
-            Org Page
-            <Form />
+        <div className="w-full mb-20">
+            <Info />
+            <Separator className="my-4" />
+            <div className="px-2 md:px-4">
+                <BoardList />
+            </div>
         </div>
     );
 };
 
-export default OrganizationPage;
+export default OrganizationIdPage;
