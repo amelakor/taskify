7247a1557e83588aa1287295e4b444b74e7bbc42
amelakor <amela_korjenic@hotmail.com>
2024-03-12 11:27:35 +0100

    Reordering list

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { useState, useEffect } from "react";
 import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+import { useAction } from "@/hooks/use-action";
+import { updateListOrder } from "@/actions/update-list-order";
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
 import { ListItem } from "./list-item";
+import { toast } from "sonner";
 
 interface ListContainerProps {
     data: ListWithCards[];
@@ -20,6 +23,15 @@ function reorder<T>(list: T[], startIndex: number, endIndex: number) {
 
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
     const [orderedData, setOrderedData] = useState(data);
+
+    const { execute: executeListOrder } = useAction(updateListOrder, {
+        onSuccess: () => {
+            toast.success("List reodered");
+        },
+        onError: (error) => {
+            toast.error(error);
+        },
+    });
 
     useEffect(() => {
         setOrderedData(data);
@@ -48,6 +60,7 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
             ).map((item, index) => ({ ...item, order: index }));
 
             setOrderedData(items);
+            executeListOrder({ items, boardId });
         }
 
         // card drag&drop
