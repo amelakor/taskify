e5cc3a1d7094160b5ea3ef052817f3d4fabac7d4
amelakor <amela_korjenic@hotmail.com>
2024-03-12 11:03:20 +0100

    DRag&Drop - frontend optimistic update

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { Card } from "@prisma/client";
-
-//import { useCardModal } from "@/hooks/use-card-modal";
+import { Draggable } from "@hello-pangea/dnd";
 
 interface CardItemProps {
     data: Card;
@@ -10,15 +9,19 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => {
-    // const cardModal = useCardModal();
-
     return (
-        <div
-            role="button"
-            //onClick={() => cardModal.onOpen(data.id)}
-            className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
-        >
-            {data.title}
-        </div>
+        <Draggable draggableId={data.id} index={index}>
+            {(provided) => (
+                <div
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                    role="button"
+                    className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+                >
+                    {data.title}
+                </div>
+            )}
+        </Draggable>
     );
 };
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
 import { ListItem } from "./list-item";
@@ -9,6 +10,14 @@ interface ListContainerProps {
     boardId: string;
 }
 
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+}
+
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
     const [orderedData, setOrderedData] = useState(data);
 
@@ -16,12 +25,113 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
         setOrderedData(data);
     }, [data]);
 
+    const onDragEnd = (result: any) => {
+        const { destination, source, type } = result;
+
+        if (!destination) return;
+
+        // if dropped in the same position
+
+        if (
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        )
+            return;
+
+        // list drag&drop
+
+        if (type === "list") {
+            const items = reorder(
+                orderedData,
+                source.index,
+                destination.index
+            ).map((item, index) => ({ ...item, order: index }));
+
+            setOrderedData(items);
+        }
+
+        // card drag&drop
+
+        if (type === "card") {
+            let newOrderedData = [...orderedData];
+
+            const sourceList = newOrderedData.find(
+                (list) => list.id === source.droppableId
+            );
+            const destinationList = newOrderedData.find(
+                (list) => list.id === destination.droppableId
+            );
+
+            if (!sourceList || !destinationList) return;
+
+            if (!sourceList.cards) {
+                sourceList.cards = [];
+            }
+
+            if (!destinationList.cards) {
+                destinationList.cards = [];
+            }
+
+            // same list
+
+            if (source.droppableId === destination.droppableId) {
+                const reorderedCards = reorder(
+                    sourceList.cards,
+                    source.index,
+                    destination.index
+                );
+
+                reorderedCards.forEach((card, index) => {
+                    card.order = index;
+                });
+
+                sourceList.cards = reorderedCards;
+
+                setOrderedData(newOrderedData);
+            } else {
+                // different list
+                const [movedCard] = sourceList.cards.splice(source.index, 1);
+
+                movedCard.listId = destination.droppableId;
+
+                destinationList.cards.splice(destination.index, 0, movedCard);
+
+                sourceList.cards.forEach((card, index) => {
+                    card.order = index;
+                });
+
+                destinationList.cards.forEach((card, index) => {
+                    card.order = index;
+                });
+
+                setOrderedData(newOrderedData);
+            }
+        }
+    };
+
     return (
-        <ol className="flex gap-x-3 h-full">
-            {orderedData.map((list, index) => {
-                return <ListItem key={list.id} index={index} data={list} />;
-            })}
-            <ListForm />
-        </ol>
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="list" type="list" direction="horizontal">
+                {(provided) => (
+                    <ol
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        className="flex gap-x-3 h-full"
+                    >
+                        {orderedData.map((list, index) => {
+                            return (
+                                <ListItem
+                                    key={list.id}
+                                    index={index}
+                                    data={list}
+                                />
+                            );
+                        })}
+                        {provided.placeholder}
+                        <ListForm />
+                    </ol>
+                )}
+            </Droppable>
+        </DragDropContext>
     );
 };
