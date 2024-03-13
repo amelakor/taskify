17e0ffbedda7547af707cfc912ffb7f3d3c2eda9
amelakor <amela_korjenic@hotmail.com>
2024-03-13 14:07:41 +0100

    Board list -test 5

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -47,19 +47,25 @@ export const BoardList = async () => {
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
                 {boards &&
                     boards?.map((board) => (
-                        <Link
+                        <p
+                            className="relative font-semibold text-white"
                             key={board?.id}
-                            href={`/board/${board?.id}`}
-                            className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
-                            style={{
-                                backgroundImage: `url(${board?.imageThumbUrl})`,
-                            }}
                         >
-                            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
-                            <p className="relative font-semibold text-white">
-                                {board?.title}
-                            </p>
-                        </Link>
+                            {board?.title}
+                        </p>
+                        // <Link
+                        //     key={board?.id}
+                        //     href={`/board/${board?.id}`}
+                        //     className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
+                        //     style={{
+                        //         backgroundImage: `url(${board?.imageThumbUrl})`,
+                        //     }}
+                        // >
+                        //     <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+                        //     <p className="relative font-semibold text-white">
+                        //         {board?.title}
+                        //     </p>
+                        // </Link>
                     ))}
                 {/* <FormPopover sideOffset={10} side="right">
                     <div
